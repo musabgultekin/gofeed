1470f6aa1bacbe956f038cf92914cbc64adf887a
Musab Gültekin <gultekinmusab@gmail.com>
2019-03-10 21:01:36 +0300

    Unescape XML entities using default html escaper

--- internal/shared/parseutils.go
+++ internal/shared/parseutils.go
@@ -1,11 +1,9 @@
 package shared
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
+	"html"
 	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/mmcdole/goxpp"
@@ -16,9 +14,6 @@ var (
 	nameEmailRgx = regexp.MustCompile(`^([^@]+)\s+\(([^@]+@[^)]+)\)$`)
 	nameOnlyRgx  = regexp.MustCompile(`^([^@()]+)$`)
 	emailOnlyRgx = regexp.MustCompile(`^([^@()]+@[^@()]+)$`)
-
-	TruncatedEntity         = errors.New("truncated entity")
-	InvalidNumericReference = errors.New("invalid numeric reference")
 )
 
 // FindRoot iterates through the tokens of an xml document until
@@ -95,78 +90,7 @@ func ParseText(p *xpp.XMLPullParser) (string, error) {
 // DecodeEntities decodes escaped XML entities
 // in a string and returns the unescaped string
 func DecodeEntities(str string) (string, error) {
-	data := []byte(str)
-	buf := bytes.NewBuffer([]byte{})
-
-	for len(data) > 0 {
-		// Find the next entity
-		idx := bytes.IndexByte(data, '&')
-		if idx == -1 {
-			buf.Write(data)
-			break
-		}
-
-		// Write and skip everything before it
-		buf.Write(data[:idx])
-		data = data[idx+1:]
-
-		if len(data) == 0 {
-			return "", TruncatedEntity
-		}
-
-		// Find the end of the entity
-		end := bytes.IndexByte(data, ';')
-		if end == -1 {
-			return "", TruncatedEntity
-		}
-
-		if data[0] == '#' {
-			// Numerical character reference
-			var str string
-			base := 10
-
-			if len(data) > 1 && data[1] == 'x' {
-				str = string(data[2:end])
-				base = 16
-			} else {
-				str = string(data[1:end])
-			}
-
-			i, err := strconv.ParseUint(str, base, 32)
-			if err != nil {
-				return "", InvalidNumericReference
-			}
-
-			buf.WriteRune(rune(i))
-		} else {
-			// Predefined entity
-			name := string(data[:end])
-
-			var c byte
-			switch name {
-			case "lt":
-				c = '<'
-			case "gt":
-				c = '>'
-			case "quot":
-				c = '"'
-			case "apos":
-				c = '\''
-			case "amp":
-				c = '&'
-			default:
-				return "", fmt.Errorf("unknown predefined "+
-					"entity &%s;", name)
-			}
-
-			buf.WriteByte(c)
-		}
-
-		// Skip the entity
-		data = data[end+1:]
-	}
-
-	return buf.String(), nil
+	return html.UnescapeString(str), nil
 }
 
 // ParseNameAddress parses name/email strings commonly
